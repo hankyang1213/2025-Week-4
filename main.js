e15510f845e7d946b4b3c2b85b323d292538e562
hank <47803174+hankyang1213@users.noreply.github.com>
2025-07-29 15:31:10 +0800

    update something

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import './assets/scss/all.scss';
 
-console.log('Hello world');
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav:true,
+    dots:false,
+        responsive:{
+            0:{
+            items:1
+            },
+            600:{
+            items:3
+            },
+            1000:{
+            items:4
+            }
+        }
+})
